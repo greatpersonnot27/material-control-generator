f21b8f6a3233f37255d14fb47729f65ef4523a60
Irakli Vashakidze <greatpersonnot27@gmail.com>
2021-05-31 15:38:09 +0400

    Add E2E tests close #4

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,51 @@ export class AppPage {
     return browser.get(browser.baseUrl);
   }
 
+  async setInputJson(): Promise<unknown> {
+    return element(by.css('#mainTextArea')).sendKeys(`{
+      "controls": [
+      {
+      "type": "Button",
+      "x": 10,
+      "y": 20,
+      "width": 80,
+      "height": 40,
+      "text": "Button"
+      },
+      {
+      "type": "Label",
+      "x": 10,
+      "y": 60,
+      "width": 80,
+      "height": 40,
+      "text": "Label"
+      }
+      ]
+     }`);
+    //  element(by.id('generateButton')).set
+  }
+
+  async clickGenerate(): Promise<unknown> {
+    return element(by.id('generateButton')).click();
+  }
+
   async getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText();
+  }
+
+  async getURL(): Promise<string> {
+    return browser.getCurrentUrl();
+  }
+
+  async getButtonName(): Promise<string> {
+    return element(by.id('generateButton')).getText();
+  }
+
+  async getButtonStatus(): Promise<boolean> {
+    return element(by.id('generateButton')).isEnabled();
+  }
+
+  async getElementByText(type: string, txt: string): Promise<string> {
+    return element(by.cssContainingText(type, txt)).getText();
   }
 }
