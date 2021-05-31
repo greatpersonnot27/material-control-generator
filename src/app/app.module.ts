677c1115529f6dc77da8223d58d33723d6c0acb8
Irakli Vashakidze <greatpersonnot27@gmail.com>
2021-05-31 11:46:47 +0400

    Add db service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/controls-display/controls-display.component.spec.ts
+++ src/app/controls-display/controls-display.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { ControlDbService } from '../control-db.service';
 import { ControlsDisplayComponent } from './controls-display.component';
 
 describe('ControlsDisplayComponent', () => {
@@ -8,9 +9,24 @@ describe('ControlsDisplayComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ControlsDisplayComponent ]
-    })
-    .compileComponents();
+      declarations: [ControlsDisplayComponent],
+      providers:
+      [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {paramMap: {id: '1', get: () => '1'}}
+          }
+        },
+        {
+          provide: ControlDbService,
+          useValue: {
+            controlsDb: ['1'],
+            getControl: () => '1'
+          }
+        }
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -25,16 +41,21 @@ describe('ControlsDisplayComponent', () => {
 
   it(`should return css style object`, () => {
     let cssParams = {
-      "type": "Button",
-      "x": 10,
-      "y": 20,
-      "width": 80,
-      "height": 40,
-      "text": "Button"
-      }
+      type: 'Button',
+      x: 10,
+      y: 20,
+      width: 80,
+      height: 40,
+      text: 'Button',
+    };
     const fixture = TestBed.createComponent(ControlsDisplayComponent);
     const jsonLoader = fixture.componentInstance;
-    expect(jsonLoader.generateCss(cssParams)).toEqual({height: "40px", left: "10px", position: "absolute", top: "20px", width: "80px"});
+    expect(jsonLoader.generateCss(cssParams)).toEqual({
+      height: '40px',
+      left: '10px',
+      position: 'absolute',
+      top: '20px',
+      width: '80px',
+    });
   });
-
 });
